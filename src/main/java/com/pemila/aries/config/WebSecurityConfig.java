8a8b19944fb2b3a92bff300009e5359b020bd777
pemila <pemila@126.com>
2020-01-19 15:56:26 +0800

    add:用户账号密码登录认证

---
+++ src/main/java/com/pemila/aries/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.pemila.aries.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * 向web页面添加安全配置，只有认证过的用户可以访问
+ * @author pemila
+ * @date 2020/1/19 15:35
+ **/
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				// `/` 及`/home`路径被设置为不需要身份认证
+					.antMatchers("/","/home").permitAll().anyRequest().authenticated()
+				.and()
+					.formLogin().loginPage("/login").permitAll()
+				.and()
+					.logout().permitAll();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.passwordEncoder(new BCryptPasswordEncoder()).withUser("user")
+				.password(new BCryptPasswordEncoder().encode("123456")).roles("USER");
+	}
+}
